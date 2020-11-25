eaf5430667517ddd11a9aa806867a97364b604aa
alluriprasad-test <74967903+alluriprasad-test@users.noreply.github.com>
2020-11-25 16:56:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 var nconf = require('nconf').argv().env()
+var pjson = require('./package.json')
 const app = express()
 
 if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging') {
@@ -18,9 +19,9 @@ app.listen(port, () => {
 })
 
 app.get('/', (req, res) => {
-  return res.send('Hello World!')
+  return res.send({message: 'Hello World!', ts: new Date().toISOString(), gitRevision: pjson['gitRevision']})
 })
 
 app.get('/healthCheck', function (req, res) {
-	return res.json({message: 'all good...'})
+	return res.json({message: 'all good...', ts: new Date().toISOString()})
 })
